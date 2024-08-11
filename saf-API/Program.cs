33740d49ec4288cc72fbd88ddbeb5925bbf6c70f
Clayton Safranek <claytonsafranek@gmail.com>
2024-08-11 12:14:24 -0500

    whitelist prod UI with cors

--- saf-API/Program.cs
+++ saf-API/Program.cs
@@ -23,6 +23,6 @@ app.UseAuthorization();
 app.MapControllers();
 
 // setup cors
-app.UseCors(builder => builder.WithOrigins("http://localhost:5173"));
+app.UseCors(builder => builder.WithOrigins("http://localhost:5173", "https://saftware-ui.azurewebsites.net/"));
 
 app.Run();
